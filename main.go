3b7fb26d80b3daba11cb5016203bf1ab710a1325
Ikenna Richard <oguejioforrichard@gmail.com>
2024-02-21 22:49:21 +0100

    Add git ignore and dot env

--- main.go
+++ main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	_ "github.com/joho/godotenv/autoload"
 	// "net/http"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "A basic crud app in Go",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
