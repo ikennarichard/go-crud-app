da8eb767ba22c3b4247ab7ecc1b2fa72e3379d1a
Ikenna Richard <oguejioforrichard@gmail.com>
2024-02-22 23:34:48 +0100

    Refactor code

--- initializers/database.go
+++ initializers/database.go
@@ -1,28 +1,27 @@
-package main
+package initializers
 
 import (
 	"log"
 	"os"
 
-	"github.com/ikennarichard/go-crud-app/models"
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func main () {
-	err := godotenv.Load()
-  if err != nil {
+var DB *gorm.DB
+var err error
+
+func ConnectDB () {
+	error := godotenv.Load()
+  if error != nil {
     log.Fatal("Error loading .env file")
   }
 
 	dsn := os.Getenv("DB_URL")
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic("failed to connect database")
 	}
-
-	// migrate the schema
-	db.AutoMigrate(&models.Employee{})
 }
